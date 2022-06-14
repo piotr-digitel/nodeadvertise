c2d9180de1af03907c7562042c3edfd9c46f38f3
Piotr Kuczkowski <digitel@o2.pl>
2022-06-14 13:36:54 +0200

    nowe pomesły

---
+++ frompb/mongo.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('mongodb');
+
+const url = process.env.MONGODB_CONNECTION;
+
+const AdvCollectionName = 'advs';
+
+let db;
+let advsCollection;
+
+const init = () =>
+    MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
+        .then((client) => {
+            db = client.db(process.env.MONGODB_DBNAME);
+            advsCollection = db.collection(AdvCollectionName);
+        })
+        .catch(error => console.log(error));
+
+
+
+const deleteAll = () => {
+    return advsCollection.deleteMany();               //deleta all documents
+}
+
+const addAdvs = (newCollection) => {           
+    return advsCollection.insertMany(newCollection,   //insert many documents to test db
+        {
+            ordered: true
+        });
+}
+
+module.exports = { init, deleteAll, addAdvs };
